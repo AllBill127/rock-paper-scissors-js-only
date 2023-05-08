15ee9f7380bb8718ca6be4be33cad6febbffe08a
AllBill127 <90201218+AllBill127@users.noreply.github.com>
2023-05-08 20:20:40 +0300

    fix 7 issues
    1. add welcome message.
    2. truncate leading whitespaces on user input.
    3. print user input on bad input error.
    4. handle cancel buttons on prompt.
    5. ask for confirmation on game cancelation.
    6. start game automatically on page load.
    7. transfer all info logging to the game()

--- js/game.js
+++ js/game.js
@@ -1,3 +1,12 @@
+// Player_Computer choice constants
+const DRAW = 0;
+const ROCK_PAPER = 1;
+const ROCK_SCISSORS = 2;
+const PAPER_SCISSORS = 3;
+const PAPER_ROCK = 4;
+const SCISSORS_ROCK = 5;
+const SCISSORS_PAPER = 6;
+
 function computerPlayer() {
     const choice = Math.floor(Math.random() * 3) + 1;
 
@@ -11,48 +20,48 @@ function computerPlayer() {
     return 
 }
 
-function player() {
+function playerInput() {
     let input = prompt("Write your choice of \"Rock\" \"Paper\" \"Scissors\"");
 
     while(true) {
-        if(input == null)
-            input = prompt("Bad input!\nWrite your choice of \"Rock\" \"Paper\" \"Scissors\"");
-        else if(input.toLowerCase() === 'x')
-            break;
-        else if(input.toLowerCase() === "rock"
-        || input.toLowerCase() === "paper"
-        || input.toLowerCase() === "scissors")
+        if(input == null) {
+            if(confirm("Are you sure you want to end the game?"))
+                break;
+
+            input = prompt("Write your choice of \"Rock\" \"Paper\" \"Scissors\"");
+        }
+        else if(input.trim().toLowerCase() === "rock"
+        || input.trim().toLowerCase() === "paper"
+        || input.trim().toLowerCase() === "scissors")
             break;
         else
-            input = prompt("Bad input!\nWrite your choice of \"Rock\" \"Paper\" \"Scissors\"");
+            input = prompt(`Bad input!\nYou entered: ${input}\nWrite your choice of \"Rock\" \"Paper\" \"Scissors\"`);
     }
 
     return input;
 }
 
 function playRound(computerChoice, playerChoice) {
-    const p = playerChoice.toLowerCase();
-    const c = computerChoice.toLowerCase();
-
-    console.log(`Computer chose: ${c}\nPlayer chose: ${p}`)
+    const p = playerChoice.trim().toLowerCase();
+    const c = computerChoice.trim().toLowerCase();
 
     if(p === c)
-        return 0;
+        return DRAW;
 
     if(p === "rock" && c === "paper")
-        return 1;
+        return ROCK_PAPER;
     if(p === "rock" && c === "scissors")
-        return 2;
+        return ROCK_SCISSORS;
 
     if(p === "paper" && c === "scissors")
-        return 3;
+        return PAPER_SCISSORS;
     if(p === "paper" && c === "rock")
-        return 4;
+        return PAPER_ROCK;
 
     if(p === "scissors" && c === "rock")
-        return 5;
+        return SCISSORS_ROCK;
     if(p === "scissors" && c === "paper")
-        return 6;
+        return SCISSORS_PAPER;
 }
 
 function game(roundCount = 5) {
@@ -61,43 +70,46 @@ function game(roundCount = 5) {
     
     let computerChoice;
     let playerChoice;
-    let roundRez;
+    let roundOutcome;
 
     for (let i = 1; i <= roundCount; ++i) {
         computerChoice = computerPlayer();
-        playerChoice = player();
-        if(playerChoice.toLowerCase() === 'x') {
+        playerChoice = playerInput();
+
+        if(playerChoice == null) {
             console.log("Game stoped before finishing!")
             return;
         }
 
         console.log(`Round ${i}/${roundCount} results:`)
+        console.log(`Computer chose: ${computerChoice}\nPlayer chose: ${playerChoice.trim()}`)
+
         roundOutcome = playRound(computerChoice, playerChoice)
         switch(roundOutcome) {
-            case 0:
+            case DRAW:
                 console.log("Draw!");
                 break;
-            case 1:
+            case ROCK_PAPER:
                 console.log("You Lose! Paper beats Rock");
                 cScore++;
                 break;
-            case 2:
+            case ROCK_SCISSORS:
                 console.log("You Win! Rock beats Scissors");
                 pScore++;
                 break;
-            case 3:
+            case PAPER_SCISSORS:
                 console.log("You Lose! Scissors beats Paper");
                 cScore++;
                 break;
-            case 4:
+            case PAPER_ROCK:
                 console.log("You Win! Paper beats Rock");
                 pScore++;
                 break;
-            case 5:
+            case SCISSORS_ROCK:
                 console.log("You Lose! Rock beats Scissors");
                 cScore++;
                 break;
-            case 6:
+            case SCISSORS_PAPER:
                 console.log("You Win! Scissors beats Paper");
                 pScore++;
                 break;
@@ -110,4 +122,17 @@ function game(roundCount = 5) {
         console.log("You won!")
     else if(pScore < cScore)
         console.log("You lost!");
+
+    if(confirm("Do you want to play again?"))
+        location.reload();
 }
+
+function gameStart() {
+    if(confirm("This is a Rock Paper Scissors game!\nDo you want to start?"))
+        game();
+    else
+        if(confirm("Second chance!\nPlay the game?"))
+            game();
+}
+
+window.addEventListener("load", gameStart());
